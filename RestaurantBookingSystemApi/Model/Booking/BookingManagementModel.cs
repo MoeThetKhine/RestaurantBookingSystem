f1081c04eee2269e2c4be47a83771a270644b305
Linn Thit <lth1212001@gmail.com>
2024-05-30 00:31:31 +0630

    -

--- RestaurantBookingSystemApi/Model/Booking/BookingManagementModel.cs
+++ RestaurantBookingSystemApi/Model/Booking/BookingManagementModel.cs
@@ -1,18 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace RestaurantBookingSystemApi.Model.Booking
+namespace RestaurantBookingSystemApi.Model.Booking;
+
+public class BookingManagementModel
 {
-    public class BookingManagementModel
-    {
-        [Key]
-        public long BookingId {  get; set; }
-        public string CustomerName { get; set; }
-        public string PhoneNumber {  get; set; }
-        public string NumberOfPeople {  get; set; }
-        public DateTime BookingDateAndTime {  get; set; }
-        public string BranchCode {  get; set; }
-        public string TableNumber {  get; set; }
-        public string UserName {  get; set; }
-        public bool IsBooked {  get; set; }
-    }
+    [Key]
+    public long BookingId {  get; set; }
+    public string CustomerName { get; set; }
+    public string PhoneNumber {  get; set; }
+    public string NumberOfPeople {  get; set; }
+    public DateTime BookingDateAndTime {  get; set; }
+    public string BranchCode {  get; set; }
+    public string TableNumber {  get; set; }
+    public string UserName {  get; set; }
+    public bool IsBooked {  get; set; }
 }
