7a08d8717fe351f01bd458d7600c9ed77afa8024
Linn Thit <lth1212001@gmail.com>
2024-05-30 00:31:41 +0630

    -

--- RestaurantBookingSystemApi/Model/Branch/BranchManagementModel.cs
+++ RestaurantBookingSystemApi/Model/Branch/BranchManagementModel.cs
@@ -1,16 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RestaurantBookingSystemApi.Model.Branch
+namespace RestaurantBookingSystemApi.Model.Branch;
+
+[Table("Branch")]
+public class BranchManagementModel
 {
-    [Table("Branch")]
-    public class BranchManagementModel
-    {
-        [Key]
-        public long BranchId { get; set; }   
-        public string BranchCode { get; set; }
-        public string BranchName { get; set; }
-        public string Location { get; set; }
-        public bool IsActive {  get; set; }
-    }
+    [Key]
+    public long BranchId { get; set; }   
+    public string BranchCode { get; set; }
+    public string BranchName { get; set; }
+    public string Location { get; set; }
+    public bool IsActive {  get; set; }
 }
