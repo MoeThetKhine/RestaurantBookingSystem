1953f25fead384975731c7ce11498757e8423b50
Linn Thit <lth1212001@gmail.com>
2024-05-30 00:30:52 +0630

    -

--- RestaurantBookingSystemApi/Model/Admin/AdminManagementModel.cs
+++ RestaurantBookingSystemApi/Model/Admin/AdminManagementModel.cs
@@ -1,18 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RestaurantBookingSystemApi.Model.Admin
+namespace RestaurantBookingSystemApi.Model.Admin;
+
+[Table("User")]
+public class AdminManagementModel
 {
-    [Table("User")]
-    public class AdminManagementModel
-    {
-        [Key]
-        public long UserId {  get; set; }
-        public string UserName { get; set; }
-        public string Email {  get; set; }
-        public string Password { get; set; }
-        public string BranchCode { get; set; }
-        public string UserRole {  get; set; }
-        public bool IsActive { get; set; }
-    }
+    [Key]
+    public long UserId {  get; set; }
+    public string UserName { get; set; }
+    public string Email {  get; set; }
+    public string Password { get; set; }
+    public string BranchCode { get; set; }
+    public string UserRole {  get; set; }
+    public bool IsActive { get; set; }
 }
