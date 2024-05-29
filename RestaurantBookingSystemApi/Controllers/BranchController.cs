9663ccc958c3c5be90be255427a01111d69f39c7
Linn Thit <lth1212001@gmail.com>
2024-05-30 00:28:36 +0630

    -

--- RestaurantBookingSystemApi/Controllers/BranchController.cs
+++ RestaurantBookingSystemApi/Controllers/BranchController.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using RestaurantBookingSystemApi.Data;
-using RestaurantBookingSystemApi.Model.Admin;
 using RestaurantBookingSystemApi.Model.Branch;
 
 namespace RestaurantBookingSystemApi.Controllers
