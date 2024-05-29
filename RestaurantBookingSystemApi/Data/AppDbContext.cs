e54bbd9b60f90e3d9da29db2c49211cafdd7f52d
Linn Thit <lth1212001@gmail.com>
2024-05-30 00:30:42 +0630

    -

--- RestaurantBookingSystemApi/Data/AppDbContext.cs
+++ RestaurantBookingSystemApi/Data/AppDbContext.cs
@@ -4,19 +4,17 @@ using RestaurantBookingSystemApi.Model.Booking;
 using RestaurantBookingSystemApi.Model.Branch;
 using RestaurantBookingSystemApi.Model.Tables;
 
-namespace RestaurantBookingSystemApi.Data
+namespace RestaurantBookingSystemApi.Data;
+
+public class AppDbContext : DbContext
 {
-
-    public class AppDbContext : DbContext
+    public AppDbContext(DbContextOptions options) : base(options)
     {
-        public AppDbContext(DbContextOptions options) : base(options)
-        {
 
-        }
-
-        public DbSet<AdminManagementModel> Users { get; set; }
-        public DbSet<BranchManagementModel> Branches { get; set; }
-        public DbSet<TablesManagementModel> Tables { get; set; }
-        public DbSet<BookingManagementModel> Booking { get; set; }
     }
+
+    public DbSet<AdminManagementModel> Users { get; set; }
+    public DbSet<BranchManagementModel> Branches { get; set; }
+    public DbSet<TablesManagementModel> Tables { get; set; }
+    public DbSet<BookingManagementModel> Booking { get; set; }
 }
