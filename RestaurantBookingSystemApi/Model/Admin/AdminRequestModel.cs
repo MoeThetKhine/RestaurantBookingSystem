bbc0ed9bab9b9431a1dd5575b1a504484b61e430
Linn Thit <lth1212001@gmail.com>
2024-05-30 00:31:14 +0630

    -

--- RestaurantBookingSystemApi/Model/Admin/AdminRequestModel.cs
+++ RestaurantBookingSystemApi/Model/Admin/AdminRequestModel.cs
@@ -1,12 +1,11 @@
-﻿namespace RestaurantBookingSystemApi.Model.Admin
+﻿namespace RestaurantBookingSystemApi.Model.Admin;
+
+public class AdminRequestModel
 {
-    public class AdminRequestModel
-    {
-        public string UserName {  get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string BranchCode { get; set; }
-        public string UserRole { get; set; }
-        public bool IsActive { get; set; }
-    }
+    public string UserName {  get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string BranchCode { get; set; }
+    public string UserRole { get; set; }
+    public bool IsActive { get; set; }
 }
