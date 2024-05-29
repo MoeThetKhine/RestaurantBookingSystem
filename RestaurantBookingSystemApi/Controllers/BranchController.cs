e026bec4fcc349240ffcc36b3792c66d8630d72b
Linn Thit <lth1212001@gmail.com>
2024-05-30 00:28:55 +0630

    -

--- RestaurantBookingSystemApi/Controllers/BranchController.cs
+++ RestaurantBookingSystemApi/Controllers/BranchController.cs
@@ -55,6 +55,7 @@ public class BranchController : ControllerBase
             throw new Exception(ex.Message);
         }
     }
+
     [HttpDelete]
     [Route("/api/Branch")]
     public async Task<IActionResult>DeleteBranches(long id)
