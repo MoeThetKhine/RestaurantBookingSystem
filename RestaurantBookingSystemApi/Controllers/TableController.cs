7ad329ce2281c19251144ec1173b3a1954fbe9c7
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 22:57:34 +0630

    -

--- RestaurantBookingSystemApi/Controllers/TableController.cs
+++ RestaurantBookingSystemApi/Controllers/TableController.cs
@@ -38,17 +38,33 @@ public class TableController : ControllerBase
     {
         try
         {
-            if (string.IsNullOrEmpty(managementModel.TableNumber) || string.IsNullOrEmpty(managementModel.Capacity) || string.IsNullOrEmpty(managementModel.Location) || string.IsNullOrEmpty(managementModel.BranchCode))
-            {
+            #region Validation
+
+            if (string.IsNullOrEmpty(managementModel.TableNumber))
                 return BadRequest();
-            }
+
+            if (string.IsNullOrEmpty(managementModel.Capacity))
+                return BadRequest();
+
+            if (string.IsNullOrEmpty(managementModel.Location))
+                return BadRequest();
+
+            if (string.IsNullOrEmpty(managementModel.BranchCode))
+                return BadRequest();
+
+            #endregion
+
             var item = await _appDbContext.Tables
                 .AsNoTracking()
-                .FirstOrDefaultAsync(x => x.TableNumber == managementModel.TableNumber && x.BranchCode == managementModel.BranchCode && x.IsAvailable);
+                .FirstOrDefaultAsync(x => x.TableNumber == managementModel.TableNumber
+                && x.BranchCode == managementModel.BranchCode && x.IsAvailable);
+
             if (item is not null)
                 return Conflict("Table Number already exists in this BranchCode!");
+
             await _appDbContext.Tables.AddAsync(managementModel);
             int result = await _appDbContext.SaveChangesAsync();
+
             return result > 0 ? StatusCode(201, "Creating Successful") : BadRequest("Creating Fail");
         }
         catch (Exception ex)
@@ -65,19 +81,24 @@ public class TableController : ControllerBase
         {
             if (string.IsNullOrEmpty(requestModel.TableNumber) || id <= 0)
                 return BadRequest();
+
             bool isDuplicate = await _appDbContext.Tables
                 .AsNoTracking()
                 .AnyAsync(x => x.TableNumber == requestModel.TableNumber && x.BranchCode == requestModel.BranchCode && x.IsAvailable && x.TableId != id);
             if (isDuplicate)
                 return Conflict("Table Number already exists");
+
             var item = await _appDbContext.Tables
                 .AsNoTracking()
                 .FirstOrDefaultAsync(x => x.TableId == id && x.IsAvailable == false);
+
             if (item is null)
                 return NotFound("Table Not Found Or IsActive");
+
             item.IsAvailable = true;
             _appDbContext.Entry(item).State = EntityState.Modified;
             int result = await _appDbContext.SaveChangesAsync();
+
             return result > 0 ? StatusCode(202, "Updating Successful") : BadRequest("Updating Fail");
         }
         catch (Exception ex)
@@ -94,14 +115,17 @@ public class TableController : ControllerBase
         {
             if (id <= 0)
                 return BadRequest();
+
             var item = await _appDbContext.Tables
                 .AsNoTracking()
                 .FirstOrDefaultAsync(x => x.TableId == id && x.IsAvailable);
             if (item is null)
                 return NotFound("Table is not available now");
+
             item.IsAvailable = false;
             _appDbContext.Entry(item).State = EntityState.Modified;
             int result = await _appDbContext.SaveChangesAsync();
+
             return result > 0 ? StatusCode(202, "Deleting Successful") : BadRequest("Deleting Fail");
         }
         catch (Exception ex)
