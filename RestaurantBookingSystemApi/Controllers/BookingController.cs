2dfd54d3f80fa8ad342b34414fa8f5177d5105e2
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 22:42:28 +0630

    -

--- RestaurantBookingSystemApi/Controllers/BookingController.cs
+++ RestaurantBookingSystemApi/Controllers/BookingController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using RestaurantBookingSystemApi.Data;
 using RestaurantBookingSystemApi.Model.Booking;
-using RestaurantBookingSystemApi.Model.Tables;
 
 namespace RestaurantBookingSystemApi.Controllers;
 
@@ -25,6 +23,7 @@ public class BookingController : ControllerBase
             List<BookingManagementModel> lst = await _appDbContext.Booking
                 .AsNoTracking()
                 .ToListAsync();
+
             return Ok(lst);
         }
         catch (Exception ex)
@@ -39,26 +38,42 @@ public class BookingController : ControllerBase
     {
         try
         {
-            if (string.IsNullOrEmpty(managementmodel.CustomerName) ||
-                string.IsNullOrEmpty(managementmodel.PhoneNumber) || 
-                string.IsNullOrEmpty(managementmodel.NumberOfPeople) ||
-                string.IsNullOrEmpty(managementmodel.BranchCode) ||
-                string.IsNullOrEmpty(managementmodel.TableNumber) || 
-                string.IsNullOrEmpty(managementmodel.UserName))
-            {
+            #region Validation
+
+            if (string.IsNullOrEmpty(managementmodel.CustomerName))
                 return BadRequest();
-            }
-            if (managementmodel.BookingDateAndTime < DateTime.Now)
-            {
+
+            if (string.IsNullOrEmpty(managementmodel.PhoneNumber))
+                return BadRequest();
+
+            if (string.IsNullOrEmpty(managementmodel.NumberOfPeople))
+                return BadRequest();
+
+            if (string.IsNullOrEmpty(managementmodel.NumberOfPeople))
+                return BadRequest();
+
+            if (string.IsNullOrEmpty(managementmodel.BranchCode))
+                return BadRequest();
+
+            if (string.IsNullOrEmpty(managementmodel.TableNumber))
+                return BadRequest();
+
+            if (string.IsNullOrEmpty(managementmodel.UserName))
+                return BadRequest();
+
+            if (managementmodel.BookingDateAndTime < DateTime.Now || managementmodel.BookingDateAndTime == default)
                 return BadRequest("Your Booking is not approved");
-            }
+
+            #endregion
+
             var item = await _appDbContext.Booking
                 .AsNoTracking()
-                .FirstOrDefaultAsync(x => x.CustomerName == managementmodel.CustomerName  && x.BookingDateAndTime == managementmodel.BookingDateAndTime && x.IsBooked);
+                .FirstOrDefaultAsync(x => x.CustomerName == managementmodel.CustomerName && x.BookingDateAndTime == managementmodel.BookingDateAndTime && x.IsBooked);
             if (item is not null)
                 return Conflict("Customer with same Booking Date/Time is already exist");
             await _appDbContext.Booking.AddAsync(managementmodel);
             int result = await _appDbContext.SaveChangesAsync();
+
             return result > 0 ? StatusCode(201, "Creating Successful") : BadRequest("Creating Fail");
         }
         catch (Exception ex)
@@ -75,14 +90,18 @@ public class BookingController : ControllerBase
         {
             if (id <= 0)
                 return BadRequest();
+
             var item = await _appDbContext.Booking
                 .AsNoTracking()
                 .FirstOrDefaultAsync(x => x.BookingId == id && x.IsBooked);
+
             if (item is null)
                 return NotFound("Booking Not Found or Active");
+
             item.IsBooked = false;
             _appDbContext.Entry(item).State = EntityState.Modified;
             int result = await _appDbContext.SaveChangesAsync();
+
             return result > 0 ? StatusCode(202, "Deleting Successful") : BadRequest("Deleting Fail");
         }
         catch (Exception ex)
