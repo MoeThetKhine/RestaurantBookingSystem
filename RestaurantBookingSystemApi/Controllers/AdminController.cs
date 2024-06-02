4edaa9c6c2b5fe4e58e5a40d497d17c061bef3ea
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-02 22:38:20 +0630

    -

--- RestaurantBookingSystemApi/Controllers/AdminController.cs
+++ RestaurantBookingSystemApi/Controllers/AdminController.cs
@@ -31,37 +31,39 @@ public class AdminController : ControllerBase
         }
     } */
 
-    [HttpGet]
-    [Route("/api/User/branchcode")]
-    public async Task<IActionResult> GetUsers(string branchcode)
-    {
-        try
-        {
-            var branch
-        }
-        catch (Exception ex)
-        {
-            throw new Exception(ex.Message);
-        }
-    }
+    //[HttpGet]
+    //[Route("/api/User/branch-code")]
+    //public async Task<IActionResult> GetUsers(string branchCode)
+    //{
+    //    try
+    //    {
+    //        var branch
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        throw new Exception(ex.Message);
+    //    }
+    //}
 
     [HttpPost]
     [Route("/api/User")]
-    public async Task<IActionResult> CreateUser([FromBody] AdminManagementModel managementmodel)
+    public async Task<IActionResult> CreateUser([FromBody] AdminManagementModel managementModel)
     {
         try
         {
-            if (string.IsNullOrEmpty(managementmodel.UserName) || string.IsNullOrEmpty(managementmodel.Email) || string.IsNullOrEmpty(managementmodel.Password) || string.IsNullOrEmpty(managementmodel.BranchCode))
+            if (string.IsNullOrEmpty(managementModel.UserName) || string.IsNullOrEmpty(managementModel.Email) || string.IsNullOrEmpty(managementModel.Password) || string.IsNullOrEmpty(managementModel.BranchCode))
             {
                 return BadRequest();
             }
+
             var item = await _appDbContext.Users
                 .AsNoTracking()
-                .FirstOrDefaultAsync(x => x.BranchCode == managementmodel.BranchCode && x.IsActive);
+                .FirstOrDefaultAsync(x => x.BranchCode == managementModel.BranchCode && x.IsActive);
             if (item is not null)
                 return Conflict("Admin already exists!");
-            await _appDbContext.Users.AddAsync(managementmodel);
+            await _appDbContext.Users.AddAsync(managementModel);
             int result = await _appDbContext.SaveChangesAsync();
+
             return result > 0 ? StatusCode(201, "Creating Successful") : BadRequest("Creating Fail");
         }
         catch (Exception ex)
@@ -72,16 +74,28 @@ public class AdminController : ControllerBase
 
     [HttpPut]
     [Route("/api/User")]
-    public async Task<IActionResult> UpdateUser([FromBody] AdminManagementModel requestmodel, long id)
+    public async Task<IActionResult> UpdateUser([FromBody] AdminManagementModel requestModel, long id)
     {
         try
         {
-            if (string.IsNullOrEmpty(requestmodel.UserName) || string.IsNullOrEmpty(requestmodel.Email) || string.IsNullOrEmpty(requestmodel.Password) || string.IsNullOrEmpty(requestmodel.BranchCode) || id <= 0)
-                //if (string.IsNullOrEmpty(requestmodel.Password) || id <= 0)
+            if (string.IsNullOrEmpty(requestModel.UserName))
                 return BadRequest();
+
+            if (string.IsNullOrEmpty(requestModel.Email))
+                return BadRequest();
+
+            if (string.IsNullOrEmpty(requestModel.Password))
+                return BadRequest();
+
+            if (string.IsNullOrEmpty(requestModel.BranchCode))
+                return BadRequest();
+
+            if (id <= 0)
+                return BadRequest();
+
             bool isDuplicate = await _appDbContext.Users
                 .AsNoTracking()
-                .AnyAsync(x => x.BranchCode == requestmodel.BranchCode && x.IsActive && x.UserId != id);
+                .AnyAsync(x => x.BranchCode == requestModel.BranchCode && x.IsActive && x.UserId != id);
             if (isDuplicate)
                 return Conflict("Admin already exists!");
             var item = await _appDbContext.Users
@@ -89,7 +103,7 @@ public class AdminController : ControllerBase
                 .FirstOrDefaultAsync(x => x.UserId == id && x.IsActive);
             if (item is null)
                 return NotFound("Admin Not Found Or Active");
-            item.Password = requestmodel.Password;
+            item.Password = requestModel.Password;
             _appDbContext.Entry(item).State = EntityState.Modified;
             int result = await _appDbContext.SaveChangesAsync();
             return result > 0 ? StatusCode(202, "Updating Successful") :
